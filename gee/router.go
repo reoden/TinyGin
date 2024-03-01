a0910d6639fe3ee075b63ba92b00ef497dbaa1a1
reoden <staraino_o@163.com>
2024-03-01 13:43:10 +0800

    Trie

--- gee/router.go
+++ gee/router.go
@@ -17,78 +17,78 @@ func newRouter() *router {
 	}
 }
 
-func parsePattern(pattern string) []string{
-  vs := strings.Split(pattern, "/")
-  
-  parts := make([]string, 0)
-  for _, part := range vs {
-    if part != "" {
-      parts = append(parts, part)
-      if strings.HasPrefix(part, "*") {
-        break
-      }
-    }
-  }
+func parsePattern(pattern string) []string {
+	vs := strings.Split(pattern, "/")
 
-  return parts
+	parts := make([]string, 0)
+	for _, part := range vs {
+		if part != "" {
+			parts = append(parts, part)
+			if strings.HasPrefix(part, "*") {
+				break
+			}
+		}
+	}
+
+	return parts
 }
 
 func (r *router) addRoute(method, pattern string, handler HandlerFunc) {
-  parts := parsePattern(pattern)
+	parts := parsePattern(pattern)
 
-  key := method + "-" + pattern
-  _, ok := r.roots[method]
-  if !ok {
-    r.roots[method] = &node{}
-  }
-  r.roots[method].insert(pattern, parts, 0)
-  r.handlers[key] = handler
+	key := method + "-" + pattern
+	_, ok := r.roots[method]
+	if !ok {
+		r.roots[method] = &node{}
+	}
+	r.roots[method].insert(pattern, parts, 0)
+	r.handlers[key] = handler
 }
 
 func (r *router) getRoute(method, path string) (*node, map[string]string) {
-  searchParts := parsePattern(path)
-  params := make(map[string]string)
-  
-  root, ok := r.roots[method]
-  if !ok {
-    return nil, nil
-  }
+	searchParts := parsePattern(path)
+	params := make(map[string]string)
 
-  n := root.query(searchParts, 0)
-  if n == nil {
-    return nil, nil
-  }
+	root, ok := r.roots[method]
+	if !ok {
+		return nil, nil
+	}
 
-  parts := parsePattern(n.pattern)
-  for i, part := range parts {
-    if strings.HasPrefix(part, ":") {
-      params[part[1:]] = searchParts[i]
-    }
-    if strings.HasPrefix(part, "*") && len(part) > 1 {
-      params[part[1:]] = strings.Join(searchParts[i:], "/")
-      break
-    }
-  }
+	n := root.query(searchParts, 0)
+	if n == nil {
+		return nil, nil
+	}
 
-  return n, params
+	parts := parsePattern(n.pattern)
+	for i, part := range parts {
+		if strings.HasPrefix(part, ":") {
+			params[part[1:]] = searchParts[i]
+		}
+		if strings.HasPrefix(part, "*") && len(part) > 1 {
+			params[part[1:]] = strings.Join(searchParts[i:], "/")
+			break
+		}
+	}
+
+	return n, params
 }
 
 func (r *router) getRoutes(method string) []*node {
-  root, ok := r.roots[method]
-  if !ok {
-    return nil
-  }
-  nodes := make([]*node, 0)
-  root.travel(&nodes)
-  return nodes
+	root, ok := r.roots[method]
+	if !ok {
+		return nil
+	}
+	nodes := make([]*node, 0)
+	root.travel(&nodes)
+	return nodes
 }
 
 func (r *router) handle(ctx *Context) {
-  n, params := r.getRoute(ctx.Method, ctx.Path)
+	n, params := r.getRoute(ctx.Method, ctx.Path)
 	if n != nil {
-    ctx.Params = params
-    key := ctx.Method + "-" + n.pattern
-    r.handlers[key](ctx)
+		ctx.Params = params
+		key := ctx.Method + "-" + n.pattern
+		r.handlers[key](ctx)
 	} else {
 		ctx.String(http.StatusNotFound, "404 NOT FOUND: %s\n", ctx.Path)
 	}
