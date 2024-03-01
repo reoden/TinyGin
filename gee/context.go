ca5c717ef5b90742b7152ddabce71fc4d458214d
reoden <staraino_o@163.com>
2024-03-01 12:48:59 +0800

    Trie

--- gee/context.go
+++ gee/context.go
@@ -13,59 +13,64 @@ type Context struct {
 	Req        *http.Request
 	Method     string
 	Path       string
+	Params     map[string]string
 	StatusCode int
 }
 
 func newContext(w http.ResponseWriter, req *http.Request) *Context {
-  return &Context{
-    Writer: w,
-    Req: req,
-    Method: req.Method,
-    Path: req.URL.Path,
-  }
+	return &Context{
+		Writer: w,
+		Req:    req,
+		Method: req.Method,
+		Path:   req.URL.Path,
+	}
+}
+
+func (ctx *Context) Param(key string) string {
+	value := ctx.Params[key]
+	return value
 }
 
 func (ctx *Context) PostForm(key string) (value string) {
-  value = ctx.Req.FormValue(key)
-  return
+	value = ctx.Req.FormValue(key)
+	return
 }
 
 func (ctx *Context) Query(key string) string {
-  return ctx.Req.URL.Query().Get(key)
+	return ctx.Req.URL.Query().Get(key)
 }
 
 func (ctx *Context) Status(code int) {
-  ctx.StatusCode = code
-  ctx.Writer.WriteHeader(code)
+	ctx.StatusCode = code
+	ctx.Writer.WriteHeader(code)
 }
 
 func (ctx *Context) SetHeader(key, value string) {
-  ctx.Writer.Header().Set(key, value)
+	ctx.Writer.Header().Set(key, value)
 }
 
 func (ctx *Context) String(code int, format string, values ...interface{}) {
-  ctx.SetHeader("Content-Type", "text/plain")
-  ctx.Status(code)
-  ctx.Writer.Write([]byte(fmt.Sprintf(format, values...)))
+	ctx.SetHeader("Content-Type", "text/plain")
+	ctx.Status(code)
+	ctx.Writer.Write([]byte(fmt.Sprintf(format, values...)))
 }
 
 func (ctx *Context) JSON(code int, obj interface{}) {
-  ctx.SetHeader("Content-Type", "application/json")
-  ctx.Status(code)
-  encoder := json.NewEncoder(ctx.Writer)
-  if err := encoder.Encode(obj); err != nil {
-    http.Error(ctx.Writer, err.Error(), 500)
-  }
+	ctx.SetHeader("Content-Type", "application/json")
+	ctx.Status(code)
+	encoder := json.NewEncoder(ctx.Writer)
+	if err := encoder.Encode(obj); err != nil {
+		http.Error(ctx.Writer, err.Error(), 500)
+	}
 }
 
 func (ctx *Context) Data(code int, data []byte) {
-  ctx.Status(code)
-  ctx.Writer.Write(data)
+	ctx.Status(code)
+	ctx.Writer.Write(data)
 }
 
 func (ctx *Context) HTML(code int, html string) {
-  ctx.SetHeader("Content-Type", "text/html")
-  ctx.Status(code)
-  ctx.Writer.Write([]byte(html))
+	ctx.SetHeader("Content-Type", "text/html")
+	ctx.Status(code)
+	ctx.Writer.Write([]byte(html))
 }
-
